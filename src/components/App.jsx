103380f3974568ff00c23b21b4c1aa9618eb7164
JorgenMU <jorgenulven@gmail.com>
2024-01-18 12:57:57 +0100

    init

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import '../App.css';
+import TekstBoks from './TekstBoks';
+
+
+function App() {
+  return (
+    <div className=" h-screen bg-slate-800">
+      <h1 className=" text-white">hade</h1>
+      <TekstBoks />
+    </div>
+    );
+}
+
+export default App;
---
+++ src/components/TekstBoks.jsx
@@ -0,0 +1,11 @@
+
+
+function TekstBoks() {
+    return (
+        <div>
+        <p className=" text-white">Detter er en tekstboks</p>
+        </div>
+    );
+}
+
+export default TekstBoks;
