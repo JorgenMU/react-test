fa7564b3d18cb2fbe664c7f0880d7e99aa2c4a0f
JorgenMU <jorgenulven@gmail.com>
2024-01-18 12:58:36 +0100

    prettier

--- src/components/TekstBoks.jsx
+++ src/components/TekstBoks.jsx
@@ -1,11 +1,9 @@
-
-
 function TekstBoks() {
-    return (
-        <div>
-        <p className=" text-white">Detter er en tekstboks</p>
-        </div>
-    );
+  return (
+    <div>
+      <p className=" text-white">Detter er en tekstboks</p>
+    </div>
+  );
 }
 
 export default TekstBoks;
