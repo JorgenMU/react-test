5f3de1ff5a3b85e9534abb2a8b0712303a20d848
JorgenMU <jorgenulven@gmail.com>
2024-01-25 11:17:41 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,304 +2,37 @@ import "../App.css";
 import Ansattkort from "./Ansattkort";
 
 function App() {
-  let ansatte =[
+  let ansatte = [
     {
-      navn: "Knut Eivind Brennhaug", 
-      tlf: "123456789", 
-      epost: "Kunt@tillermail.com"},
-    { 
+      navn: "Knut Eivind Brennhaug",
+      tlf: "123456789",
+      epost: "Kunt@tillermail.com",
+    },
+    {
       navn: "Roar Gjerde",
       tlf: "4363456789",
-      epost: "Roar@tillermail.com"
+      epost: "Roar@tillermail.com",
     },
     {
       navn: "Bent Erik Hammernes",
       tlf: "4345766789",
-      epost: "BentErik@tillermail.com"
-    },
-    {
-      navn: "Jon Pape Hallem",
-      tlf: "56746789",
-      epost: "Jon@tillermail.com"
-    },
-    {
-      navn: "Håvard Heggem",
-      tlf: "5674r4565434565453426789",
-      epost: "Håvard@tillermail.com"
-    },
-    {
-      navn: "Knut Eivind Brennhaug", 
-      tlf: "123456789", 
-      epost: "Kunt@tillermail.com"},
-    { 
-      navn: "Roar Gjerde",
-      tlf: "4363456789",
-      epost: "Roar@tillermail.com"
-    },
-    {
-      navn: "Bent Erik Hammernes",
-      tlf: "4345766789",
-      epost: "BentErik@tillermail.com"
-    },
-    {
-      navn: "Jon Pape Hallem",
-      tlf: "56746789",
-      epost: "Jon@tillermail.com"
-    },
-    {
-      navn: "Håvard Heggem",
-      tlf: "5674r4565434565453426789",
-      epost: "Håvard@tillermail.com"
-    },
-    {
-      navn: "Knut Eivind Brennhaug", 
-      tlf: "123456789", 
-      epost: "Kunt@tillermail.com"},
-    { 
-      navn: "Roar Gjerde",
-      tlf: "4363456789",
-      epost: "Roar@tillermail.com"
-    },
-    {
-      navn: "Bent Erik Hammernes",
-      tlf: "4345766789",
-      epost: "BentErik@tillermail.com"
-    },
-    {
-      navn: "Jon Pape Hallem",
-      tlf: "56746789",
-      epost: "Jon@tillermail.com"
-    },
-    {
-      navn: "Håvard Heggem",
-      tlf: "5674r4565434565453426789",
-      epost: "Håvard@tillermail.com"
-    },
-    {
-      navn: "Knut Eivind Brennhaug", 
-      tlf: "123456789", 
-      epost: "Kunt@tillermail.com"},
-    { 
-      navn: "Roar Gjerde",
-      tlf: "4363456789",
-      epost: "Roar@tillermail.com"
-    },
-    {
-      navn: "Bent Erik Hammernes",
-      tlf: "4345766789",
-      epost: "BentErik@tillermail.com"
-    },
-    {
-      navn: "Jon Pape Hallem",
-      tlf: "56746789",
-      epost: "Jon@tillermail.com"
-    },
-    {
-      navn: "Håvard Heggem",
-      tlf: "5674r4565434565453426789",
-      epost: "Håvard@tillermail.com"
-    },
-    {
-      navn: "Knut Eivind Brennhaug", 
-      tlf: "123456789", 
-      epost: "Kunt@tillermail.com"},
-    { 
-      navn: "Roar Gjerde",
-      tlf: "4363456789",
-      epost: "Roar@tillermail.com"
-    },
-    {
-      navn: "Bent Erik Hammernes",
-      tlf: "4345766789",
-      epost: "BentErik@tillermail.com"
-    },
-    {
-      navn: "Jon Pape Hallem",
-      tlf: "56746789",
-      epost: "Jon@tillermail.com"
-    },
-    {
-      navn: "Håvard Heggem",
-      tlf: "5674r4565434565453426789",
-      epost: "Håvard@tillermail.com"
-    },
-    {
-      navn: "Knut Eivind Brennhaug", 
-      tlf: "123456789", 
-      epost: "Kunt@tillermail.com"},
-    { 
-      navn: "Roar Gjerde",
-      tlf: "4363456789",
-      epost: "Roar@tillermail.com"
-    },
-    {
-      navn: "Bent Erik Hammernes",
-      tlf: "4345766789",
-      epost: "BentErik@tillermail.com"
-    },
-    {
-      navn: "Jon Pape Hallem",
-      tlf: "56746789",
-      epost: "Jon@tillermail.com"
-    },
-    {
-      navn: "Håvard Heggem",
-      tlf: "5674r4565434565453426789",
-      epost: "Håvard@tillermail.com"
-    },
-    {
-      navn: "Knut Eivind Brennhaug", 
-      tlf: "123456789", 
-      epost: "Kunt@tillermail.com"},
-    { 
-      navn: "Roar Gjerde",
-      tlf: "4363456789",
-      epost: "Roar@tillermail.com"
-    },
-    {
-      navn: "Bent Erik Hammernes",
-      tlf: "4345766789",
-      epost: "BentErik@tillermail.com"
-    },
-    {
-      navn: "Jon Pape Hallem",
-      tlf: "56746789",
-      epost: "Jon@tillermail.com"
-    },
-    {
-      navn: "Håvard Heggem",
-      tlf: "5674r4565434565453426789",
-      epost: "Håvard@tillermail.com"
-    },
-    {
-      navn: "Knut Eivind Brennhaug", 
-      tlf: "123456789", 
-      epost: "Kunt@tillermail.com"},
-    { 
-      navn: "Roar Gjerde",
-      tlf: "4363456789",
-      epost: "Roar@tillermail.com"
-    },
-    {
-      navn: "Bent Erik Hammernes",
-      tlf: "4345766789",
-      epost: "BentErik@tillermail.com"
-    },
-    {
-      navn: "Jon Pape Hallem",
-      tlf: "56746789",
-      epost: "Jon@tillermail.com"
-    },
-    {
-      navn: "Håvard Heggem",
-      tlf: "5674r4565434565453426789",
-      epost: "Håvard@tillermail.com"
-    },
-    {
-      navn: "Knut Eivind Brennhaug", 
-      tlf: "123456789", 
-      epost: "Kunt@tillermail.com"},
-    { 
-      navn: "Roar Gjerde",
-      tlf: "4363456789",
-      epost: "Roar@tillermail.com"
-    },
-    {
-      navn: "Bent Erik Hammernes",
-      tlf: "4345766789",
-      epost: "BentErik@tillermail.com"
-    },
-    {
-      navn: "Jon Pape Hallem",
-      tlf: "56746789",
-      epost: "Jon@tillermail.com"
-    },
-    {
-      navn: "Håvard Heggem",
-      tlf: "5674r4565434565453426789",
-      epost: "Håvard@tillermail.com"
-    },
-    {
-      navn: "Knut Eivind Brennhaug", 
-      tlf: "123456789", 
-      epost: "Kunt@tillermail.com"},
-    { 
-      navn: "Roar Gjerde",
-      tlf: "4363456789",
-      epost: "Roar@tillermail.com"
-    },
-    {
-      navn: "Bent Erik Hammernes",
-      tlf: "4345766789",
-      epost: "BentErik@tillermail.com"
-    },
-    {
-      navn: "Jon Pape Hallem",
-      tlf: "56746789",
-      epost: "Jon@tillermail.com"
-    },
-    {
-      navn: "Håvard Heggem",
-      tlf: "5674r4565434565453426789",
-      epost: "Håvard@tillermail.com"
-    },
-    {
-      navn: "Knut Eivind Brennhaug", 
-      tlf: "123456789", 
-      epost: "Kunt@tillermail.com"},
-    { 
-      navn: "Roar Gjerde",
-      tlf: "4363456789",
-      epost: "Roar@tillermail.com"
-    },
-    {
-      navn: "Bent Erik Hammernes",
-      tlf: "4345766789",
-      epost: "BentErik@tillermail.com"
-    },
-    {
-      navn: "Jon Pape Hallem",
-      tlf: "56746789",
-      epost: "Jon@tillermail.com"
-    },
-    {
-      navn: "Håvard Heggem",
-      tlf: "5674r4565434565453426789",
-      epost: "Håvard@tillermail.com"
-    },
-    {
-      navn: "Knut Eivind Brennhaug", 
-      tlf: "123456789", 
-      epost: "Kunt@tillermail.com"},
-    { 
-      navn: "Roar Gjerde",
-      tlf: "4363456789",
-      epost: "Roar@tillermail.com"
-    },
-    {
-      navn: "Bent Erik Hammernes",
-      tlf: "4345766789",
-      epost: "BentErik@tillermail.com"
-    },
-    {
-      navn: "Jon Pape Hallem",
-      tlf: "56746789",
-      epost: "Jon@tillermail.com"
-    },
-    {
-      navn: "Håvard Heggem",
-      tlf: "5674r4565434565453426789",
-      epost: "Håvard@tillermail.com"
+      epost: "BentErik@tillermail.com",
     },
   ];
   return (
     <div className="h-screen bg-slate-800">
-      <h1 className="text-4xl text-center text-white py-5">Ansatte på tiller</h1>
+      <h1 className="text-4xl text-center text-white py-5">
+        Ansatte på tiller
+      </h1>
       <div className="grid h-screen bg-slate-800 grid-cols-3 ">
-
-        {ansatte.map(ansatte =>(
-          <Ansattkort navn={ansatte.navn} tlf={ansatte.tlf} epost={ansatte.epost}/>
+        {ansatte.map((ansatte) => (
+          <Ansattkort
+            navn={ansatte.navn}
+            tlf={ansatte.tlf}
+            epost={ansatte.epost}
+          />
         ))}
+        <h1 className=" text-blue-400">dette er en test</h1>
       </div>
     </div>
   );
